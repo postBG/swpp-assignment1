60e17124ec7b08ad1525bf0e15c024c0aec7043f
postBG <profile2697@gmail.com>
2015-10-01 16:22:03 +0900

    마이그레이션 파일과 모델 파일 작성하고 테이블 만들기

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,7 @@
+class UserController < ApplicationController
+  def login
+  end
+
+  def login_complete
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   validates :username, :presence => { :messages => "Username cannot be blank." }
   validates :username, :uniqueness => { :messages => "Username is already taken."}
-  validates :username, :length => { :minimum => 5, :maximum => 20 }
-  validates :password, :length => { :minimum => 8, :maximum => 20 }
+  validates :username, :length => { :minimum => 5, :maximum => 20, :too_short =>"Your name must be at least 5 characters long.", :too_long => "Your name must be at most 20 characters long." }
+  validates :password, :length => { :minimum => 8, :maximum => 20, :too_short =>"Your password must be at least 8 characters long.", :too_long => "Your password must be at most 20 characters long." }
 end
